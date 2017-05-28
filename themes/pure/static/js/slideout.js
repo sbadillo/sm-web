38fd14b4d3db88ecea4d9b69d9635760e675ea7d
Sergio <sbadillo@gmail.com>
2017-05-28 17:36:26 +0200

    dunno

---
+++ themes/pure/static/js/slideout.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	// Script that adds slideout property to whole content section
+	
+	$('#activate-slideout').click(function() {
+
+		console.log($(this).attr("class")); 		// reads classes of clicked element
+
+	    $('.content').addClass('slideout');			// slidesout content element
+
+
+
+	});
+
+	// Need to do this for each element: 
+	// 1. get name/slug of project on click, maybe the slug is included as the class 
+	// 2. pull project description from md file <-- jinja?
+	// 3. populates a div with that description.
+	// 4. 
+
+
+})
+
+
+
